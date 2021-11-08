245d19a3ba5c55e5760c58db864c6ea1e60d7159
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-11-08 16:41:51 -0300

    operador ternario, função anonima

---
+++ JavaScrypt/Exercicios/1Fundamentos/exercise10.js
@@ -0,0 +1,8 @@
+/*
+* 10)Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retor* ne true ou false.
+*/
+
+
+(function (numero){
+	console.log(numero % 3 == 0 ? true: false)
+})(9)
