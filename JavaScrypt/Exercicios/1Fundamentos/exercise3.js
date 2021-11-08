525a356dfaaee81bbf4d1995efc417d98a0a6c19
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-11-08 14:24:33 -0300

    Create exercise3.js

---
+++ JavaScrypt/Exercicios/1Fundamentos/exercise3.js
@@ -0,0 +1,9 @@
+/*
+03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+
+(function(base,exponent){
+	console.log(`base: ${base} exponent: ${exponent} = ${Math.pow(base,exponent)}`)
+})(2,6)
+
+//Feito com uma função auto invocada
