57982d9b7ef38478b3c0910ad78521058a823793
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-11-10 20:05:01 -0300

    Create 4exercise.js

---
+++ JavaScrypt/Exercicios/2Fundamentos/4exercise.js
@@ -0,0 +1,7 @@
+/*
+Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo
+*/
+
+const checkFirstIsMoreBiggerThanSecond = (number1, number2) => number1>=number2?true:false
+
+console.log(checkFirstIsMoreBiggerThanSecond(5,10))
