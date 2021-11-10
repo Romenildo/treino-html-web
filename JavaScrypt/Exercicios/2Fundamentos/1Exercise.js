3e907a5a3a2c10528d1b5e5efc826feb9a37a599
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-11-10 19:47:31 -0300

    Create 1Exercise.js

---
+++ JavaScrypt/Exercicios/2Fundamentos/1Exercise.js
@@ -0,0 +1,7 @@
+/*Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+função) e com ponto de exclamação "!" no final.
+*/
+
+const cumprimentar= text => "Olá "+ text + "!"
+
+console.log(cumprimentar("Ronaldo"))
